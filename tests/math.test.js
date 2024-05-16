cdfd77a03a2323c427bb665b60f98f81e3dcc85b
darklatiz <software.j2ee@gmail.com>
2024-05-15 23:03:40 -0500

    ci/cd - code check

--- tests/math.test.js
+++ tests/math.test.js
@@ -8,7 +8,7 @@ test('adds 1 + 2 to equal 3', () => {
 });
 
 test('subtracts 5 - 2 to equal 3', () => {
-    expect(subtract(5, 2)).toBe(67);
+    expect(subtract(5, 2)).toBe(3);
 });
 
 test('multiplies 3 * 4 to equal 12', () => {
